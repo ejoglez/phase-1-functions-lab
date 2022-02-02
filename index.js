6a77caa616d42cfac85b54adf8a93a8456c1abdf
Eric Gonzalez <ejoglez@gmail.com>
2022-02-01 19:28:59 -0600

    commit tree c28239f688eb8cf77349356d71d2e950c00cf837

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(location){
+    let x = location - 42; let y = Math.abs(x);
+    return y;
+}
+
+
+function distanceFromHqInFeet(location){
+    let x = distanceFromHqInBlocks(location) * 264; 
+    return x;
+}
+
+function distanceTravelledInFeet(start, end){
+    let x = (start - end) * 264;
+    let y = Math.abs(x);
+    return y;
+}
+
+function calculatesFarePrice(start, end){
+    let x = distanceTravelledInFeet(start,end);
+    
+    if (x <= 400){
+        return 0;
+    } 
+    if ((x > 400) && (x < 2000)) {
+        return (x-400) * .02;
+    }    
+    if ((x > 2000) && (x < 2500)) {
+        return 25;
+    }
+    else if (x > 2500) {
+        return 'cannot travel that far';
+    }
+    
+}
+
